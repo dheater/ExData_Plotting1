26f65648666a09e33ee473d43d0cafac69007897
Daniel Heater <daniel@heatertech.com>
2014-07-13 18:30:54 -0500

    Set plot width and height

--- src/plot1.R
+++ src/plot1.R
@@ -1,5 +1,5 @@
 plot1 <- function(data) {
-    png(filename="figure/plot1.png", bg="transparent")
+    png(filename="figure/plot1.png", bg="transparent", height=480, width=480)
     with(data, hist(as.numeric(Global_active_power)/1000, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power"))
     dev.off()
 }
