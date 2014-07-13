258351106c18143764739c940b105c82f504f240
Daniel Heater <daniel@heatertech.com>
2014-07-13 18:18:28 -0500

    Read and subset data

--- src/read_data.R
+++ src/read_data.R
@@ -1,3 +1,5 @@
-read_data <= function read_data() {
+read_data <- function() {
     epc=read.table("data/household_power_consumption.txt", sep=";", header=T)
+    epc$Date=as.Date(epc$Date, format="%d/%m/%Y")
+    sub.epc=subset(epc, Date>=as.Date("2007-02-01") & Date<=as.Date("2007-02-02"))
 }
