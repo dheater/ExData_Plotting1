c29e5bef3ca4789106c6385b704fae7ba478860b
Daniel Heater <daniel@heatertech.com>
2014-07-13 21:06:51 -0500

    Found error in data. Problem converting factors to numerics

--- src/plot1.R
+++ src/plot1.R
@@ -1,5 +1,5 @@
 plot1 <- function(data) {
     png(filename="figure/plot1.png", bg="transparent", height=480, width=480)
-    with(data, hist(as.numeric(Global_active_power)/1000, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power"))
+    with(data, hist(as.numeric(as.character(Global_active_power)), col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power"), breaks=12)
     dev.off()
 }
